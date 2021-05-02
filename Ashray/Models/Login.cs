e031af619d23d99bca432face6cfec058f82cb8e
Saritha Katta <saritha.katta@realpage.com>
2021-05-02 09:48:40 +0530

    adding models

---
+++ Ashray/Models/Login.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ashray.Models
+{
+    public class Login
+    {   
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty(PropertyName = "mobileNo")]
+        public string MobileNo { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty(PropertyName = "password")]
+        public string Password { get; set; }
+    }
+}
