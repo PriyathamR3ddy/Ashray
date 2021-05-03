c0dfdce009afcadfd687ecb3b157dfe9c3467d53
Mukesh Madhu <mukesh.madhu@RealPage.com>
2021-05-03 18:38:34 +0530

    removed error for files

--- Ashray/Controllers/HomeController.cs
+++ Ashray/Controllers/HomeController.cs
@@ -155,13 +155,13 @@ namespace Ashray.Controllers
             {
                 foreach (var file in files)
                 {
-                    if (file.ContentLength > 0)
-                    {
-                        var fileName = Path.GetFileName(file.FileName);
-                        var path = Path.Combine(pathname, fileName);
-                        file.SaveAs(path);
-                    }
-                }
+					if (file != null && file.ContentLength > 0)
+					{
+						var fileName = Path.GetFileName(file.FileName);
+						var path = Path.Combine(pathname, fileName);
+						file.SaveAs(path);
+					}
+				}
             }
             var res = Registation.InsertPatientHistory(ph);
             return RedirectToAction("PostHospital");
