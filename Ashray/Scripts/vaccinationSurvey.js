ea4a9ab634d27dfd254165fe01bf0bc08af6864f
Tarun Goonuru <tarun.goonuru@realpage.com>
2021-06-22 16:51:57 +0530

    Added script for vaccinated drive

---
+++ Ashray/Scripts/vaccinationSurvey.js
@@ -0,0 +1,18 @@
+﻿
+function changeAreYouVaccinatedYes() {
+    $("#vaccinatednoblock").hide();
+}
+
+function changeAreYouVaccinatedNo() {
+    $("#vaccinatednoblock").show();
+}
+
+function changeAreYouWillingToGetVaccine() {
+    var select = document.getElementById("areyouwillingtogetvaccine");
+    var answer = select.options[select.selectedIndex].value;
+    if (answer == "Yes") {
+        $("#nearestclusterblock").show();
+    } else {
+        $("#nearestclusterblock").hide();
+    }
+}
